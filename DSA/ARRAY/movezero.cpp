c0c798b746fa1c349736dc4788aca0c7342d99b8
iamshreeyyy <karmankarshrey@gmail.com>
2025-07-24 23:29:05 +0530

    movezero function add

---
+++ DSA/ARRAY/movezero.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int>temp;
+        for(int i=0;i<nums.size();i++){
+            if (nums[i]!=0){
+                temp.push_back(nums[i]);
+            }
+        }
+            for (int i=0;i<nums.size();i++){
+                if(i<temp.size()){
+                    nums[i]=temp[i];
+                }
+                else {nums[i]=0;}
+            }
+        }
+        
+    
+};
