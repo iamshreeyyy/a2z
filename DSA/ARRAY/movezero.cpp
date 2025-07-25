ba84a5b51c71f994011af20775902d0998727247
iamshreeyyy <karmankarshrey@gmail.com>
2025-07-25 12:51:38 +0530

    movezero 2nd approch

--- DSA/ARRAY/movezero.cpp
+++ DSA/ARRAY/movezero.cpp
@@ -20,3 +20,31 @@ public:
         
     
 };
+
+
+class Solution {
+public:
+    void moveZeroes(std::vector<int>& nums) {
+        int j = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        // If no zeros were found, j will be -1.
+        // The array is already correct, so we can stop.
+        if (j == -1) {
+            return;
+        }
+
+        // This part will now only run if a zero was actually found.
+        for (int i = j + 1; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[j], nums[i]);
+                j++;
+            }
+        }
+    }
+};
